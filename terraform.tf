38252665c8f1f07d08a6e0877dfece2a2c2c0dd7
bayyavuz <143806417+bayyavuz@users.noreply.github.com>
2023-09-18 16:41:31 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -8,10 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  # Configuration options
-}
-
-provider "aws" {
   region  = "us-east-1"
   access_key = "$(secrets.AWS_ACCESS_KEY_ID)"
   secret_key = "$(secrets.AWS_SECRET_ACCESS_KEY)"
