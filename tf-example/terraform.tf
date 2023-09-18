879ad16ef0236039e7a5e6a85c6bcbef5b5781e5
bayyavuz <143806417+bayyavuz@users.noreply.github.com>
2023-09-18 20:35:36 +0300

    Update terraform.tf

--- tf-example/terraform.tf
+++ tf-example/terraform.tf
@@ -37,7 +37,7 @@ resource "aws_instance" "tf-ec2" {
 }
 
 resource "aws_security_group" "github-ac-sg" {
-  name = "github-securutyg"
+  name = "github-securutygroup"
   description = "Allow HTTP and SSH traffic via Terraform"
 
   ingress {
