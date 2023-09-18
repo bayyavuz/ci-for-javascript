62ed9392a80910e0afdc6685cce65597d1814fac
bayyavuz <143806417+bayyavuz@users.noreply.github.com>
2023-09-18 20:30:54 +0300

    Update terraform.tf

--- tf-example/terraform.tf
+++ tf-example/terraform.tf
@@ -1,17 +1,10 @@
 terraform {
-  backend "remote" {
-    organization = "glich-stream"
-
-    workspaces {
-      name = "ci-cd-staging"
-    }
-  }
-
   required_providers {
     aws = {
       source = "hashicorp/aws"
       version = "5.17.0"
     }
+  }
 }
 
 provider "aws" {
