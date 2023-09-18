bd1c0cb466cdaae2b4a71fb69cbc7dd260cdb7d5
bayyavuz <143806417+bayyavuz@users.noreply.github.com>
2023-09-18 16:39:20 +0300

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -13,14 +13,14 @@ provider "aws" {
 
 provider "aws" {
   region  = "us-east-1"
-  access_key = "${{ secrets.AWS_ACCESS_KEY_ID }}"
-  secret_key = "${{ secrets.AWS_SECRET_ACCESS_KEY }}"
+  access_key = "$(secrets.AWS_ACCESS_KEY_ID)"
+  secret_key = "$(secrets.AWS_SECRET_ACCESS_KEY)"
   ## profile = "my-profile"
 }
 
 resource "aws_instance" "tf-ec2" {
   ami      = data.aws_ami.ubuntu.id
-  key_name = " ${{ secrets.SSH_PRIVATE_KEY }}" 
+  key_name = " $(secrets.SSH_PRIVATE_KEY)" 
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.demo-sg.id]
   tags = {
